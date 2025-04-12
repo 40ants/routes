7a727c808ea00cb46ed00d10b16b1e7f9f492cb3
Alexander Artemenko <svetlyak.40wt@gmail.com>
2025-04-12 19:18:34 +0300

    One more.

--- t/handler.lisp
+++ t/handler.lisp
@@ -8,6 +8,8 @@
                 #:fmt)
   (:import-from #:40ants-routes/with-url
                 #:with-url)
+  (:import-from #:40ants-routes/handler
+                #:call-handler)
   (:shadowing-import-from #:40ants-routes/defroutes
                           #:get
                           #:include
@@ -30,7 +32,7 @@
 
 (defun check-url (url expected-handler-result)
   (with-url (*app* url)
-    (let* ((result (40ants-routes/handler:call-handler))
+    (let* ((result (call-handler))
            (matchedp (equal result
                             expected-handler-result)))
       (ok matchedp
