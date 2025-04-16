8583b4d4fbebc9c7edc29c4ced21344ee8f33d73
Alexander Artemenko <svetlyak.40wt@gmail.com>
2025-04-17 00:10:32 +0300

    Removed unnecessary code created by LLM.

--- t/add-route.lisp
+++ t/add-route.lisp
@@ -10,52 +10,39 @@
                 #:children-routes
                 #:add-route)
   (:import-from #:40ants-routes/route
+                #:route-name
                 #:route)
   (:import-from #:40ants-routes/url-pattern
                 #:parse-url-pattern)
   (:import-from #:40ants-routes/errors
                 #:namespace-duplication-error
-                #:path-duplication-error))
+                #:path-duplication-error)
+  (:import-from #:40ants-routes/defroutes
+                #:include)
+  (:shadowing-import-from #:40ants-routes/defroutes
+                          #:get))
 (in-package #:40ants-routes-tests/add-route)
 
-;; Load included-routes after the package definition to avoid circular dependencies
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (asdf:load-system :40ants-routes/included-routes))
-
-(defun make-test-route (path &key (method :get) (name "test"))
-  "Helper function to create a test route"
-  (make-instance 'route
-                 :name name
-                 :pattern (parse-url-pattern path)
-                 :method method
-                 :handler (lambda () nil)))
-
-(defun make-test-included-routes (namespace path)
-  "Helper function to create test included-routes"
-  (let ((routes (make-instance 'routes
-                               :namespace namespace
-                               :children (list (make-test-route "/")))))
-    (make-instance '40ants-routes/included-routes:included-routes
-                   :original-collection routes
-                   :path (parse-url-pattern path))))
 
 (deftest test-add-route-basic ()
   "Test basic route addition functionality"
   (testing "Adding a route to an empty routes collection"
-    (let* ((routes (make-instance 'routes :namespace "app"))
-           (route (make-test-route "/test")))
-      (add-route routes route)
+    (let* ((routes (routes ("app")))
+           (route (get ("/test"))))
+      (add-route routes
+                 route)
       (ok (= (length (children-routes routes)) 1)
           "Route was successfully added")
       (ok (eq (first (children-routes routes)) route)
           "The added route is the same object"))))
 
+
 (deftest test-add-route-duplicate-path ()
   "Test adding a route with a duplicate path"
   (testing "Adding a route with a duplicate path should signal an error"
-    (let* ((routes (make-instance 'routes :namespace "app"))
-           (route1 (make-test-route "/test"))
-           (route2 (make-test-route "/test" :name "test2")))
+    (let* ((routes (routes ("app")))
+           (route1 (get ("/test")))
+           (route2 (get ("/test" :name "test2"))))
       (add-route routes route1)
       (handler-case
           (progn
@@ -64,12 +51,18 @@
         (path-duplication-error ()
           (ok t "Correctly raised path-duplication-error"))))))
 
+
 (deftest test-add-route-duplicate-namespace ()
   "Test adding included-routes with a duplicate namespace"
   (testing "Adding included-routes with a duplicate namespace should signal an error"
-    (let* ((routes (make-instance 'routes :namespace "app"))
-           (included1 (make-test-included-routes "blog" "/blog"))
-           (included2 (make-test-included-routes "blog" "/blog2")))
+    (let* ((routes (routes ("app")))
+           (included1 (include (routes ("blog")
+                                 (get ("/")))
+                               :path "/blog"))
+           ;; Here path is different, but namespace is the same - "blog"
+           (included2 (include (routes ("blog")
+                                 (get ("/")))
+                               :path "/blog2")))
       (add-route routes included1)
       (handler-case
           (progn
@@ -78,18 +71,20 @@
         (namespace-duplication-error ()
           (ok t "Correctly raised namespace-duplication-error"))))))
 
+
 (deftest test-add-route-with-override ()
   "Test adding a route with override=true"
   (testing "Adding a route with override=true should replace the existing route"
-    (let* ((routes (make-instance 'routes :namespace "app"))
-           (route1 (make-test-route "/test" :name "test1"))
-           (route2 (make-test-route "/test" :name "test2")))
+    (let* ((routes (routes ("app")))
+           (route1 (get ("/test" :name "test1")))
+           (route2 (get ("/test" :name "test2"))))
       (add-route routes route1)
       (add-route routes route2 :override t)
       (ok (= (length (children-routes routes)) 1)
           "Only one route exists after override")
-      (ok (string= (40ants-routes/route:route-name (first (children-routes routes))) "test2")
+      (ok (string= (route-name (first (children-routes routes))) "test2")
           "The new route replaced the old one"))))
+
 
 ;; Skip this test for now since it's causing issues
 (deftest test-add-included-routes-with-override ()
